2feb3d895376ad358b16ee384d76cf355b343fc7
samFabrice <dsamfabrice5@gmail.com>
2021-02-17 13:09:14 +0100

    updated api.py file

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 # -*-coding: utf-*-
 import file
 tab = []
-arr = []
-
 def load():
     return file.content
 
@@ -15,20 +13,21 @@ class Tableau:
     def affiche(self, tab):
         for i in range(len(tab)):
             for j in range(len(tab[i])):
-                print(tab[i][j], end = ' ')
+                print(tab[i][j], end  =' ')
             print()
-        
+                
     def stockerDansTab(self):
         for x in range(self.ligne):
+            sousTab = []
             for y in range(self.colonne):
-                arr.append(self.data)
-            tab.append(arr)
+                sousTab.append(self.data)
+            tab.append(sousTab)
 
 if __name__=="__main__":
     
     x = load()
 
-    tableau = Tableau(10,5,1)
+    tableau = Tableau(5,5,0)
     tableau.stockerDansTab()
     tableau.affiche(tab)
    
