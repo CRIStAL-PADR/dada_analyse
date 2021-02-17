f7607c0e1bc17499935d3b22fcd778273c43bbc8
samFabrice <dsamfabrice5@gmail.com>
2021-02-17 16:48:57 +0100

    updated api.py file

--- api.py
+++ api.py
@@ -1,33 +1,61 @@
 # -*-coding: utf-*-
 import file
-tab = []
+
 def load():
     return file.content
 
 class Tableau:
-    def __init__(self, ligne, colonne, data):
+    def __init__(self, ligne, colonne, defaultValue = 0):
         self.ligne = ligne
         self.colonne = colonne
-        self.data = data
-
-    def affiche(self, tab):
-        for i in range(len(tab)):
-            for j in range(len(tab[i])):
-                print(tab[i][j], end  =' ')
+        self.defaultValue = defaultValue
+        self.data = []
+        
+    def compteColone(self, c, value):
+        """ retourne le nombre de foix ou sur une ligne 'l' il y a la valeur "value" """
+        count = 0
+        for i  in range(len(self.data)):
+            for j in range(len(self.data[c])):
+                if (value == self.data[i][c]):
+                    count = count+1
+                    break
+        return count
+    
+    def compterLigne(self, l, value):
+        """retourne le nombre de foix ou sur une ligne 'l' il y a la valeur "value" """
+        count = 0
+        for i  in range(len(self.data)):
+            for j in range(len(self.data[l])):
+                if (value == self.data[l][j]):
+                    count = count+1
+                    break
+        return count
+          
+    
+    def affiche(self):
+        for i in range(len(self.data)):
+            for j in range(len(self.data[i])):
+                print(self.data[i][j], end  =' ')
             print()
                 
-    def stockerDansTab(self):
+    def initialize(self):
         for x in range(self.ligne):
             sousTab = []
             for y in range(self.colonne):
-                sousTab.append(self.data)
-            tab.append(sousTab)
+                sousTab.append(self.defaultValue)
+            self.data.append(sousTab)
 
 if __name__=="__main__":
     
     x = load()
 
-    tableau = Tableau(5,5,0)
-    tableau.stockerDansTab()
-    tableau.affiche(tab)
-   
+    tableau = Tableau(9,5, 'r')
+    tableau.initialize()
+    tableau.data[0][2] = 0
+    tableau.data[1][2] = 7
+    tableau.data[2][2] = 7
+    tableau.data[5][2] = 7
+    tableau.affiche()
+  
+    print(tableau.compteColone(2,7))
+    print(tableau.compterLigne(2,7))
