fad852cf004f0f68dede662a55fe4abcbe0f63c1
Damien Marchal <damien.marchal@univ-lille1.fr>
2021-02-23 12:24:32 +0100

    Les modifs

--- file.py
+++ file.py
@@ -2,7 +2,7 @@ content={
     "file1.cpp" : ["file1.h", "file3.h", "file1.cpp"],
     "file2.cpp" : ["file3.h", "file2.cpp"],
     "file3.cpp" : ["file3.h", "file3.cpp"],
-    "file1.h"   :    [" file1.cpp"],
+    "file1.h"   :    ["file1.cpp"],
     "file2.h"   :    ["file2.h"],
     "file3.h"   :    ["file3.h"],
     "file4.h"   :    ["file4.h"],
