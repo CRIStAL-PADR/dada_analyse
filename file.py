c6151558c82cb2efa31b6d10f1cdf2e90d98bb7c
samFabrice <dsamfabrice5@gmail.com>
2021-02-23 12:31:31 +0100

    updated file.py

--- file.py
+++ file.py
@@ -2,14 +2,15 @@ content={
     "file1.cpp" : ["file1.h", "file3.h", "file1.cpp"],
     "file2.cpp" : ["file3.h", "file2.cpp"],
     "file3.cpp" : ["file3.h", "file3.cpp"],
-    "file1.h"   :    [" file1.cpp"],
+    "file1.h"   :    ["file1.cpp"],
     "file2.h"   :    ["file2.h"],
     "file3.h"   :    ["file3.h"],
     "file4.h"   :    ["file4.h"],
     "monFichierAvecUnGrandNom.h"   :    ["file4.h", "file5.h","file1.h"],
     "/home/damien/development/grand/grand/fichier/monFichierAvecUnGrandNom.h"   :    ["file4.h", "file5.h","file1.h"],
     "file5.h"   :    ["file4.h", "file5.h","file1.h"],
-    "file6.h"   :    ["file4.h", "file5.h","file1.h"],
+    "file6.h"   :    ["file4.h", "file8.cpp","file1.h"],
     "file7.h"   :    ["file4.h", "file5.h","file1.h"],
-    "file8.h"   :    ["file4.h", "file5.h","file1.h"]
-}
+    "file8.cpp"   :    ["file4.h", "file5.h","file1.h"],
+    "file10.cpp"   :    ["file4.h", "file5.h","file1.h","file1.cpp"]
+   }
