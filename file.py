1944202925bcf53d2979ec13e98b10b09cd92044
samFabrice <dsamfabrice5@gmail.com>
2021-02-24 15:42:17 +0100

    updated file.py

---
+++ file.py
@@ -0,0 +1,13 @@
+content={
+    "file1.cpp" : ["file1.h", "file3.h", "file1.cpp"],
+    "file2.cpp" : ["file3.h", "file2.cpp"],
+    "file3.cpp" : ["file3.h", "file3.cpp"],
+    "file1.h"   :    ["file1.cpp"],
+    "file2.h"   :    ["file2.h", "file4.h", "file5.h"],
+    "file3.h"   :    ["file3.h","monFichierAvecUnGrandNom.h"],
+    "file4.h"   :    ["file4.h","file1.cpp"],
+    "monFichierAvecUnGrandNom.h"   :    ["file4.h", "file5.h","file1.h"],
+    "/home/damien/development/grand/fichier/monFichierAvecUnGrandNom.h"   :    ["file4.h", "file5.h","file1.h","file1.cpp"],
+    "file5.h"   :    ["file4.h", "file5.h","file1.h"],
+    "file8.cpp"   :    ["file4.h", "file5.h","file1.h"]
+   }
