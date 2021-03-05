8bbfcea495d59f8195ca363bd6023b9997575f07
samFabrice <dsamfabrice5@gmail.com>
2021-03-05 18:02:48 +0100

    updated  load function in test.py

--- test.py
+++ test.py
@@ -1,25 +1,21 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Mar  5 09:58:02 2021
-
-@author: fabrice
-"""
-
 import include_graph as file
 
 
 def load():
-
     i = 'sources'
     dictionary = {}
     file.content
+    liste  = []
     for  key in file.content.keys():
         for j in file.content[key].keys():
             if j == i:
                 for a in file.content[key][j]:
-                    dictionary[key] = a #cette partie est à revoir
+                    liste.append(a)  
+                dictionary[key] = liste
+                liste = []
+
     print(dictionary)
+            
     return dictionary
-    
+
 load()
