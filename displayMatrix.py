1daf56c9a9fc2313ec4b3332104ad87a38cc5a07
samFabrice <kdedo@polytech-lille.net>
2021-04-14 14:44:28 +0200

    I went back to the previous version of PrintInHtmlFormat and I replaced list = oldtableau.getList () by list = tableau.indexToKey to be able to display the reduced size matrix in html

--- displayMatrix.py
+++ displayMatrix.py
@@ -1,36 +1,37 @@
-def PrintInHtmlFormat(oldtableau, newtableau):
-    liste = oldtableau.getList()
-    liste = liste[:30]
+def PrintInHtmlFormat(tableau):
+    liste = tableau.indexToKey
     print ("""
-           <html>
-           <table width="70%" border = " 5" align="center" >
-           <link ref = "stylesheet" href = "style.css">
-           <tr>""")
+    <html>
+    <table width="70%" border = " 5" align="center" >
+    <link ref = "stylesheet" href = "style.css">
+        <tr>
+    """)
     for x in range(len(liste)):
         if x ==0:
             print('<td>&nbsp;</td>')
             print('<td>&nbsp;</td>')
-            print( '<td class = "td">{}</td> '.format(x))
-        elif x  <=9 and x >0:
+            print( '<td class = "td">{}</td> '.format(x), end = ' ')
+        if x <=9 and x>0:
             print('<td>{}</td> '.format(x), end = ' ')
-        else : 
+        if x >=10:
             print( '<td>{}</td> '.format(x) , end = ' ')
     print("""
           </tr>
           """)
-    for i in range(len(newtableau.data)):
-        c = newtableau.compterLigne(i)
-        keyA = oldtableau.indexToKey[i]
-        keyB = oldtableau.keyToIndex[keyA]
+    for i in range(len(tableau.data)):
+        c = tableau.compterLigne(i)
+        keyA = tableau.indexToKey[i]
+        keyB = tableau.keyToIndex[keyA]
         print(""" <tr> """)
-        print('<td>{}</td>'.format(keyA[-74:]),'<td>{}</td>'.format(keyB))
-        for j in range(len(newtableau.data[i])):
-            if newtableau.data[i][j] ==None:
+        print('<td>{}</td>'.format(keyA),'<td>{}</td>'.format(keyB))
+        for j in range(len(tableau.data[i])):
+            if tableau.data[i][j] ==None:
                 print('<td>-</td>', end  = ' ')
             else:
-                print('<td>{}</td>'.format( newtableau.data[i][j]) , end  = ' ')
+                print('<td>{}</td>'.format( tableau.data[i][j]) , end  = ' ')
         print('<td>{}</td>'.format(c))
-    print(""" </tr> """)
+    print(""" <tr> """)
     print("""
-          </table>
-          </html>""")
+    </table>
+    </h
+    """)
