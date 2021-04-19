8bde3f93da99c986df9358bee6f37e952ebe10c3
samFabrice <kdedo@polytech-lille.net>
2021-04-19 09:27:59 +0200

    I created a dispayImage function which allows to create an empty image and write on it

--- displayImage.py
+++ displayImage.py
@@ -1,5 +1,12 @@
 # -*-coding: utf-*-
-import imgkit
-
-with open('test.html') as f:
-    imgkit.from_file(f, 'tableau.jpg')
+from PIL import Image
+from PIL import ImageDraw
+ 
+img = Image.new('RGB', (1000, 1000), color = (73, 109, 137))
+ 
+d = ImageDraw.Draw(img)
+for i in range(100):
+    for j in range(100):
+        d.text((i*10,j*10), "1",  fill=(200, 255, 0), spacing = 1)
+ 
+img.save('pil_text_font.jpg')
